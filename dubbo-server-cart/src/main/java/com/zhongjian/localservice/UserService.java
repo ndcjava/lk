505e1405c390ec768fae3602c052f400fbbf621b
chendi <15395068264@163.com>
2019-06-28 09:44:56 +0800

    commit

---
+++ dubbo-server-cart/src/main/java/com/zhongjian/localservice/UserService.java
@@ -0,0 +1,8 @@
+package com.zhongjian.localservice;
+
+import com.zhongjian.dao.entity.cart.user.UserBean;
+
+public interface UserService {
+
+	Integer add(UserBean user);
+}
---
+++ dubbo-server-cart/src/main/java/com/zhongjian/localservice/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.zhongjian.localservice.impl;
+
+import com.zhongjian.dao.entity.cart.user.UserBean;
+import com.zhongjian.localservice.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public Integer add(UserBean user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
